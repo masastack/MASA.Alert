5f85f0922e606d6574ded5e8311414f77d04d2c6
wzh425 <wuzihao425@outlook.com>
2022-10-28 14:28:48 +0800

    refactor: Process Code Review

--- src/Infrastructure/Masa.Alert.EntityFrameworkCore/EntityFrameworkCore/AlertDbContext.cs
+++ src/Infrastructure/Masa.Alert.EntityFrameworkCore/EntityFrameworkCore/AlertDbContext.cs
@@ -1,15 +1,14 @@
-﻿namespace Masa.Alert.EntityFrameworkCore.EntityFrameworkCore
-{
-    public class AlertDbContext : IsolationDbContext
-    {
-        public AlertDbContext(MasaDbContextOptions<AlertDbContext> options) : base(options)
-        {
-        }
+﻿namespace Masa.Alert.EntityFrameworkCore.EntityFrameworkCore;
 
-        protected override void OnModelCreatingExecuting(ModelBuilder builder)
-        {
-            builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
-            base.OnModelCreatingExecuting(builder);
-        }
+public class AlertDbContext : IsolationDbContext
+{
+    public AlertDbContext(MasaDbContextOptions<AlertDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreatingExecuting(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        base.OnModelCreatingExecuting(builder);
     }
 }
