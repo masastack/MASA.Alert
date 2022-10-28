c31fa9b6b6d533a5d312610e1f5c77092a97c613
wzh425 <wuzihao425@outlook.com>
2022-10-28 13:47:33 +0800

    temp

---
+++ src/Web/Masa.Alert.Web.Admin/Pages/AlarmHistory/AlarmHistoryManagement.razor.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Alert.Web.Admin.Pages.AlarmHistory;
+
+public partial class AlarmHistoryManagement : AdminCompontentBase
+{
+    private bool advanced = false;
+    private bool isAnimate;
+
+    protected override string? PageName { get; set; } = "AlarmHistory";
+}
