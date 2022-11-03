d2a2c566e05c8472c6d6d60a881ba70eec2bbabe
wzh425 <wuzihao425@outlook.com>
2022-11-03 17:46:00 +0800

    feature : LinkTrackingTopology

---
+++ src/Web/Masa.Alert.Web.Admin/Pages/DataV/Modules/LinkTrackingTopologyEdgeViewModel.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Alert.Web.Admin.Pages.DataV.Modules;
+
+public class LinkTrackingTopologyEdgeViewModel
+{
+    public string Source { get; set; } = default!;
+
+    public string Target { get; set; } = default!;
+
+    public string Label { get; set; } = default!;
+}
---
+++ src/Web/Masa.Alert.Web.Admin/Pages/DataV/Modules/LinkTrackingTopologyNodeViewModel.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Alert.Web.Admin.Pages.DataV.Modules;
+
+public class LinkTrackingTopologyNodeViewModel
+{
+    public string Id { get; set; } = default!;
+
+    public string Label { get; set; } = default!;
+
+    public int Depth { get; set; }
+
+    public LinkTrackingTopologyStatuses State { get; set; }
+
+    public int X { get; set; }
+
+    public int Y { get; set; }
+}
