9269b1377222586150693669dc6a928ecd2ae12f
wzh425 <wuzihao425@outlook.com>
2022-11-22 09:57:08 +0800

    chore : from main

--- src/Application/Masa.Alert.Application.Contracts/AlarmHistories/Dtos/AlarmHandleStatusCommitDto.cs
+++ src/Application/Masa.Alert.Application.Contracts/AlarmHistories/Dtos/AlarmHandleStatusCommitDto.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-namespace Masa.Alert.Application.Contracts.AlarmHistorys.Dtos;
+namespace Masa.Alert.Application.Contracts.AlarmHistories.Dtos;
 
 public class AlarmHandleStatusCommitDto
 {
--- src/Domain/Masa.Alert.Domain/AlarmHistories/Aggregates/AlarmHandleStatusCommit.cs
+++ src/Domain/Masa.Alert.Domain/AlarmHistories/Aggregates/AlarmHandleStatusCommit.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-namespace Masa.Alert.Domain.AlarmHistorys.Aggregates;
+namespace Masa.Alert.Domain.AlarmHistories.Aggregates;
 
 public class AlarmHandleStatusCommit : ValueObject
 {
