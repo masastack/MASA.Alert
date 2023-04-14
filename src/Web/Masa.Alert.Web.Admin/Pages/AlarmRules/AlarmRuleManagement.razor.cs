0c0dc7c562929e91ee9ffc94a47defbb50457244
Kogel <935368322@qq.com>
2023-04-14 20:15:56 +0800

    fix:update SAutoCompleteSelection

--- src/Web/Masa.Alert.Web.Admin/Pages/AlarmRules/AlarmRuleManagement.razor.cs
+++ src/Web/Masa.Alert.Web.Admin/Pages/AlarmRules/AlarmRuleManagement.razor.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.Contrib.SearchEngine.AutoComplete.ElasticSearch;
-
 namespace Masa.Alert.Web.Admin.Pages.AlarmRules;
 
 public partial class AlarmRuleManagement : AdminCompontentBase
