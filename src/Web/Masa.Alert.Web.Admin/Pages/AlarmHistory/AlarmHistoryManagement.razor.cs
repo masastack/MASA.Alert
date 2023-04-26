b936c9ece3322b53767bf203461821036962f754
wzh425 <wuzihao425@outlook.com>
2023-04-26 20:02:11 +0800

    chore : global using

--- src/Web/Masa.Alert.Web.Admin/Pages/AlarmHistory/AlarmHistoryManagement.razor.cs
+++ src/Web/Masa.Alert.Web.Admin/Pages/AlarmHistory/AlarmHistoryManagement.razor.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using System.Reflection.PortableExecutable;
-
 namespace Masa.Alert.Web.Admin.Pages.AlarmHistory;
 
 public partial class AlarmHistoryManagement : AdminCompontentBase
