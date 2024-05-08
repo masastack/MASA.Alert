2616e072c8d1930569d6b73cdfbb0a4a289899ac
qinyouzeng <qinyouzeng@masastack.com>
2024-05-08 13:36:44 +0800

    fix: delivering traceid from scheduler to alarm

--- src/Application/Masa.Alert.Application/AlarmRules/Jobs/CheckAlarmRuleJob.cs
+++ src/Application/Masa.Alert.Application/AlarmRules/Jobs/CheckAlarmRuleJob.cs
@@ -1,22 +1,30 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-namespace Masa.Alert.Application.AlarmRules.Jobs
+namespace Masa.Alert.Application.AlarmRules.Jobs;
+
+public class CheckAlarmRuleJob : BackgroundJobBase<CheckAlarmRuleJobArgs>
 {
-    public class CheckAlarmRuleJob : BackgroundJobBase<CheckAlarmRuleJobArgs>
+    private readonly IEventBus _eventBus;
+    public static ActivitySource ActivitySource { get; private set; } = new("Masa.Alert.Background");
+
+    public CheckAlarmRuleJob(ILogger<BackgroundJobBase<CheckAlarmRuleJobArgs>>? logger
+        , IEventBus eventBus) : base(logger)
     {
-        private readonly IEventBus _eventBus;
+        _eventBus = eventBus;
+    }
 
-        public CheckAlarmRuleJob(ILogger<BackgroundJobBase<CheckAlarmRuleJobArgs>>? logger
-            , IEventBus eventBus) : base(logger)
+    protected async override Task ExecutingAsync(CheckAlarmRuleJobArgs args)
+    {
+        var command = new CheckAlarmRuleCommand(args.AlarmRuleId, args.ExcuteTime);
+        var activity = string.IsNullOrEmpty(args.TraceParent) ? default : ActivitySource.StartActivity("", ActivityKind.Consumer, args.TraceParent);
+        try
         {
-            _eventBus = eventBus;
-        }
-
-        protected override async Task ExecutingAsync(CheckAlarmRuleJobArgs args)
-        {
-            var command = new CheckAlarmRuleCommand(args.AlarmRuleId, args.ExcuteTime);
             await _eventBus.PublishAsync(command);
+        }
+        finally
+        {
+            activity?.Dispose();
         }
     }
 }
---
+++ src/Services/Masa.Alert.Service/Extensions/OpenTelemetryAlertExtensions.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class OpenTelemetryAlertExtensions
+{
+    public static IServiceCollection AddAlertObservable(this IServiceCollection services,
+       ILoggingBuilder loggingBuilder,
+       Func<MasaObservableOptions> optionsConfigure,
+       Func<string>? otlpUrlConfigure = null)
+    {
+        var options = optionsConfigure();
+        var otlpUrl = otlpUrlConfigure?.Invoke() ?? string.Empty;
+
+        ArgumentNullException.ThrowIfNull(options);
+
+        Uri? uri = null;
+        if (!string.IsNullOrEmpty(otlpUrl) && !Uri.TryCreate(otlpUrl, UriKind.Absolute, out uri))
+            throw new UriFormatException($"{nameof(otlpUrl)}:{otlpUrl} is invalid url");
+
+        var resources = ResourceBuilder.CreateDefault().AddMasaService(options);
+        loggingBuilder.AddMasaOpenTelemetry(builder =>
+        {
+            builder.SetResourceBuilder(resources);
+            builder.AddOtlpExporter(otlp => otlp.Endpoint = uri);
+        });
+
+        services.AddMasaMetrics(builder =>
+        {
+            builder.SetResourceBuilder(resources);
+            builder.AddOtlpExporter(options =>
+            {
+                options.Endpoint = uri;
+            });
+        });
+
+        services.AddMasaTracing(
+            builder =>
+            {
+                builder.SetResourceBuilder(resources);
+                builder.AddOtlpExporter(options => options.Endpoint = uri);
+                builder.AddSource(CheckAlarmRuleJob.ActivitySource.Name);
+            },
+            builder =>
+            {
+                builder.AspNetCoreInstrumentationOptions.AppendDefaultFilter(builder, false);
+            });
+
+        return services;
+    }
+}
