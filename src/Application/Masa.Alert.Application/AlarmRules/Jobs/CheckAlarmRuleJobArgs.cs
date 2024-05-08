bc556abde5afb16cd9689b98a547ee27fd402ab3
Qinyouzeng <102203523+Qinyouzeng@users.noreply.github.com>
2024-05-08 13:47:01 +0800

    Fix: fix delivering traceId from scheduler (#344)
    * fix: delivering traceid from scheduler to alarm
    
    * chore: update

--- src/Application/Masa.Alert.Application/AlarmRules/Jobs/CheckAlarmRuleJobArgs.cs
+++ src/Application/Masa.Alert.Application/AlarmRules/Jobs/CheckAlarmRuleJobArgs.cs
@@ -1,12 +1,13 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-namespace Masa.Alert.Application.AlarmRules.Jobs
-{
-    public class CheckAlarmRuleJobArgs
-    {
-        public Guid AlarmRuleId {  get; set; }
+namespace Masa.Alert.Application.AlarmRules.Jobs;
 
-        public DateTimeOffset? ExcuteTime { get; set; }
-    }
+public class CheckAlarmRuleJobArgs
+{
+    public Guid AlarmRuleId {  get; set; }
+
+    public DateTimeOffset? ExcuteTime { get; set; }
+
+    public string? TraceParent { get; set; }
 }
