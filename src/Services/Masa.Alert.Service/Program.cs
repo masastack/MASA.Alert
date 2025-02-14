a1e0a75e6445446c5b5db140f430f067e0eb7af1
wzh425 <wuzihao425@outlook.com>
2025-02-14 09:50:33 +0800

    chore: global using

--- src/Services/Masa.Alert.Service/Program.cs
+++ src/Services/Masa.Alert.Service/Program.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.Alert.Service.Admin.Infrastructure.Authentication;
-using Masa.BuildingBlocks.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 await builder.Services.AddMasaStackConfigAsync(MasaStackProject.Alert, MasaStackApp.Service);
